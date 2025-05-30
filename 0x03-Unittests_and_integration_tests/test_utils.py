a36bb2fd7d6826301dcd85e3900b59295a2748f7
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-30 06:31:32 +0000

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,7 +67,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 """Memoized property"""
                 return self.a_method()
-        
+
         with patch.object(
                 TestClass, 'a_method', return_value=42) as mock_method:
             test_instance = TestClass()
