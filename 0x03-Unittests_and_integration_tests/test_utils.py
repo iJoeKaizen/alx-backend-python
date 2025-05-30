70d5f4489d253cc30e4e7978d05961150291997a
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-05-30 06:03:12 +0000

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
 """
-Utility functions module
-"""
-import requests
-from functools import wraps
-
-def access_nested_map(nested_map, path):
-    """
-    Access nested dictionary with given path
-    """
-    for key in path:
-        if not isinstance(nested_map, dict) or key not in nested_map:
-            raise KeyError(key)
-        nested_map = nested_map[key]
-    return nested_map
-
-def get_json(url):
-    """
-    Fetch JSON from URL
-    """#!/usr/bin/env python3
-"""
 Unit test module for utils module
 """
 import unittest
@@ -100,20 +80,3 @@ class TestMemoize(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    response = requests.get(url)
-    return response.json()
-
-def memoize(func):
-    """
-    Memoization decorator to cache method results
-    """
-    cache = {}
-    
-    @wraps(func)
-    def wrapper(*args):
-        if args in cache:
-            return cache[args]
-        result = func(*args)
-        cache[args] = result
-        return result
-    return wrapper
