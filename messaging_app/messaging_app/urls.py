cb66cf3bcfc114cdae3ad9d1fed50dd79c7406f3
Gideon Mensah <142912117+iJoeKaizen@users.noreply.github.com>
2025-06-08 21:01:33 +0000

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
@@ -13,7 +14,12 @@ urlpatterns = [
     # DRF browsable API login/logout
     path('api-auth/', include('rest_framework.urls')),
 
+    
+
     # JWT authentication
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
